d58ff597ec0e22a9cd0c1e5d97c188bb913e8071
Sourjya9015 <sourjya9007@gmail.com>
2017-04-19 15:08:02 -0400

    Declaring some variables

--- accessPoint.m
+++ accessPoint.m
@@ -6,10 +6,14 @@ classdef accessPoint < hgsetget
         location;
         numClusters;
         topology; % stores the network topology = coordinates of every node
-        %  declare this as a Map with the key as the cluster index c1, c2,
-        %  c3 etc.
+                  %  declare this as a Map with the key as the cluster index c1, c2,
+                  %  c3 etc.
         
         cqiFeedback; % channel quality feedback; again a Map set from the main simulation
+        
+        % Expert setting
+        numExperts; % Number of experts
+        expertWt;   % Weights on each expert
         
     end
     
